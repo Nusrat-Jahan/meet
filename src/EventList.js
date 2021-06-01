e35dfb3f8bf3e34d98e856c96caa10e081181aa1
Nusrat-Jahan <jahannusrat735@gmail.com>
2021-06-01 20:01:21 +0200

    Updated files

--- src/EventList.js
+++ src/EventList.js
@@ -4,12 +4,14 @@ import Event from './Event';
 class EventList extends Component {
   render() {
     const { events } = this.props;
+    console.log(events);
     return (
       <ul className="EventList">
         {events.map(event =>
           <li key={event.id}>
             <Event event={event} />
           </li>
+
         )}
       </ul>
     );
