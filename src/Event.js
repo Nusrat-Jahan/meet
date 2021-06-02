e81fdb01b5eb687a30f4ee28659c12cc2453c266
Nusrat-Jahan <jahannusrat735@gmail.com>
2021-06-02 12:45:40 +0200

    Added show-hide details button and show details info

--- src/Event.js
+++ src/Event.js
@@ -1,8 +1,51 @@
 import React, { Component } from "react";
 
 class Event extends Component {
+  state = {
+    isExpanded: false,
+  };
+
+  handleShowHideButton = () => {
+    if (this.state.showHideDetails === true) {
+      this.setState({ showHideDetails: false });
+    } else {
+      this.setState({ showHideDetails: true });
+    }
+  };
+
   render() {
-    return <div></div>;
+    const { event } = this.props;
+    return (
+      <div className="event" onClick={() => this.handleShowHideButton()}>
+        <h1>{event.summary}</h1>
+        <p>
+          {new Date(event.start.dateTime).toLocaleDateString("en-gb", {
+            year: "numeric",
+            month: "short",
+            day: "numeric",
+            timeZone: "utc",
+          })}
+          ,
+          {new Date(event.start.dateTime).toLocaleTimeString("en-gb", {
+            time: "numeric",
+          })}
+        </p>{" "}
+        <p className="locations">{event.location}</p>
+        {this.state.showHideDetails && (
+          <div className="event-details">
+            <h2>About event:</h2>
+            <a href={event.htmlLink}>See Details on Google Calendar</a>
+            <p>{event.description}</p>
+          </div>
+        )}
+        <button
+          className="show-hide-btn"
+          onClick={() => this.handleShowHideButton()}
+        >
+          {!this.state.showHideDetails ? "show details" : "hide-details"}
+        </button>
+      </div>
+    );
   }
 }
 export default Event;
