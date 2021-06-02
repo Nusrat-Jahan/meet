4dc0b65036e6ae34dde425e89327cb2a4a462691
Nusrat-Jahan <jahannusrat735@gmail.com>
2021-06-02 13:00:12 +0200

    Added EventList and Citysearch props to work app.js

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import React, { Component } from 'react';
 import "./App.css";
 import EventList from './EventList';
 import CitySearch from './CitySearch';
-import NumberOfEvents from "./NumberOfEvents";
-import { getEvents, extractLocations } from "./api";
-import { WarningAlert } from "./Alert";
 
 class App extends Component {
+  state = {
+    events: [],
+    locations: []
+  }
   render() {
     return (
       <div className="App">
-        <CitySearch />
+        <CitySearch locations={this.state.locations} />
         <EventList events={this.state.events} />
       </div>
     );
   }
 }
 
+export default App;
