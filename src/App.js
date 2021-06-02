c1ee440d3cf864b8c6526938e79d1bd79814acb3
Nusrat-Jahan <jahannusrat735@gmail.com>
2021-06-02 12:38:54 +0200

    Passed props to eventlist to remove error map is undefined

--- src/App.js
+++ src/App.js
@@ -2,16 +2,23 @@ import React, { Component } from 'react';
 import "./App.css";
 import EventList from './EventList';
 import CitySearch from './CitySearch';
+import NumberOfEvents from "./NumberOfEvents";
+import { getEvents, extractLocations } from "./api";
+import { WarningAlert } from "./Alert";
 
 class App extends Component {
+  state = {
+    events: [],
+    locations: [],
+    eventCount: 32,
+  };
   render() {
     return (
       <div className="App">
         <CitySearch />
-        <EventList />
+        <EventList events={this.state.events} />
       </div>
     );
   }
 }
 
-export default App;
