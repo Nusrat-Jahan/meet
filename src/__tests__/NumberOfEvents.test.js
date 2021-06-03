d751ac6376f53ed145588a6248e707b2df6223cd
Nusrat-Jahan <jahannusrat735@gmail.com>
2021-06-03 12:49:49 +0200

    Removed unnecessary updateEvents

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -6,7 +6,7 @@ describe("<NumberOfEvents /> component", () => {
   let NumberOfEventsWrapper;
   beforeAll(() => {
     NumberOfEventsWrapper = shallow(
-      <NumberOfEvents updateEvents={() => { }} />);
+      <NumberOfEvents />);
   });
 
   test("render of number of events", () => {
@@ -17,10 +17,14 @@ describe("<NumberOfEvents /> component", () => {
     expect(NumberOfEventsWrapper.state("numberOfEvents")).toBe(32);
   });
 
+  test('check input value is between to 32', () => {
+    expect(NumberOfEventsWrapper.state("numberOfEvents")).toBe(32);
+  });
+
   test("change state when the number of events changes", () => {
-    // NumberOfEventsWrapper.setState({
-    //   numberOfEvents: 4,
-    // });
+    NumberOfEventsWrapper.setState({
+      numberOfEvents: 4,
+    });
     const eventObject = { target: { value: 8 } };
     NumberOfEventsWrapper.find(".NumberOfEvents input").simulate(
       "change",
