7578b88899a3edea51f8338b04d1e3abd19e2918
Nusrat-Jahan <jahannusrat735@gmail.com>
2021-06-03 12:44:03 +0200

    Removed console.log from EventList.js

--- src/EventList.js
+++ src/EventList.js
@@ -4,7 +4,7 @@ import Event from './Event';
 class EventList extends Component {
   render() {
     const { events } = this.props;
-    console.log(events);
+    // console.log(events);
     return (
       <ul className="EventList">
         {events.map(event =>
