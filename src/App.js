8e1f9feca93acaf0d34796a2d093de09018110f5
Nusrat-Jahan <jahannusrat735@gmail.com>
2021-06-04 18:22:58 +0200

    Added citySearch updateEvents

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
+import { extractLocations, getEvents } from './api';
+import './nprogress.css';
 // import Event from './Event';
 
 class App extends Component {
@@ -12,12 +14,41 @@ class App extends Component {
     numberOfEvents: [],
     // showDetails: []
   }
+
+  updateEvents = (location) => {
+    getEvents().then((events) => {
+      const locationEvents = (location === 'all') ?
+        events :
+        events.filter((event) => event.location === location);
+      this.setState({
+        events: locationEvents
+      });
+    });
+  }
+
+  componentDidMount() {
+    this.mounted = true;
+    getEvents().then((events) => {
+      if (this.mounted) {
+        this.setState({ events, locations: extractLocations(events) });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.mounted = false;
+  }
+
   render() {
     return (
       <div className="App">
-        <CitySearch locations={this.state.locations} />
+        <h1 className='header-title'>Meet App</h1>
+        <h4 className='filter-title'>Choose your nearest city</h4>
+        <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
+        {/*  pass the locations and updateEvents method as a prop to CitySearch */}
+        <NumberOfEvents numberOfEvents={this.state.numberOfEvents} updateEvents={this.updateEvents} />
         <EventList events={this.state.events} />
-        <NumberOfEvents numberOfEvents={this.state.numberOfEvents} />
+        {/* pass the state to EventList, as a prop of events */}
         {/* <Event showDetails={this.state.showDetails} /> */}
       </div>
     );
