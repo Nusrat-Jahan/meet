45bdbb3c67ce04827e2bded99c55c5d1e98467f1
Nusrat-Jahan <jahannusrat735@gmail.com>
2021-06-10 18:50:24 +0200

    Tried to solve CORS error for mac in handler.js

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -85,6 +85,9 @@ module.exports.getAccessToken = async (event) => {
       // Respond with OAuth token
       return {
         statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
         body: JSON.stringify(token),
       };
     })
@@ -93,6 +96,9 @@ module.exports.getAccessToken = async (event) => {
       console.error(err);
       return {
         statusCode: 500,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
         body: JSON.stringify(err),
       };
     });
@@ -130,6 +136,9 @@ module.exports.getCalendarEvents = async (event) => {
       // Respond with OAuth token
       return {
         statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
         body: JSON.stringify({ events: results.data.items }),
       };
     })
