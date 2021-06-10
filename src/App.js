6924ba6d280eb6de8578c47d9cd9160b75bba132
Nusrat-Jahan <jahannusrat735@gmail.com>
2021-06-10 16:28:17 +0200

    Deleted unnecessary codes from app.js

--- src/App.js
+++ src/App.js
@@ -12,118 +12,33 @@ class App extends Component {
     events: [],
     locations: [],
     numberOfEvents: 32,
-    selectedLocation: 'all',
-    currentLocation: 'all',
+    selectedLocation: 'all'
   }
-  // // working
-  // updateEvents = (location) => {
-  //   getEvents().then((events) => {
-  //     const locationEvents = (location === 'all') ?
-  //       events :
-  //       events.filter((event) => event.location === location);
-  //     const { numberOfEvents } = this.state;
-  //     const filteredEvents = locationEvents.slice(0, numberOfEvents);
-  //     const eventsByLocation = locationEvents.length;
-  //     this.setState({
-  //       events: filteredEvents,
-  //       eventsByLocation: eventsByLocation,
-  //       currentLocation: location,
-  //     });
-  //   });
-  // }
-  // updateEventCount = eventCount => {
-  //   const { currentLocation } = this.state;
-  //   this.setState({
-  //     numberOfEvents: eventCount
-  //   });
-  //   this.updateEvents(currentLocation);
-  // }
 
-
-
-  // updateEvents = (location, eventCount) => {
-  //   const { eventsByLocation, filteredEvents } = this.state;
-  //   if (location) {
-  //     getEvents().then((events) => {
-  //       // const { locationEvents } = this.state;
-  //       const locationEvents = (location === 'all') ?
-  //         events :
-  //         events.filter((event) => event.location === location);
-  //       const { numberOfEvents } = this.state;
-
-  //       const filteredEvents = events.slice(0, numberOfEvents);
-  //       const eventsByLocation = events.length;
-  //       console.log(filteredEvents);
-  //     });
-  //   }
-  //   else {
-  //     getEvents().then((events) => {
-  //       const filteredEvents = events.slice(0, eventCount);
-  //       const eventsByLocation = events.length;
-  //       console.log(filteredEvents);
-  //     });
-  //   }
-  //   this.setState({
-  //     events: filteredEvents,
-  //     currentLocation: location,
-  //     numberOfEvents: eventCount,
-  //     eventsByLocation: eventsByLocation,
-  //   });
-  // }
-
-  // updateEvents = (updatedLocation, eventCount) => {
-
-  //   const { selectedLocation, events } = this.state;
-  //   if (updatedLocation) {
-  //     // console.log("updatedLocation", updatedLocation);
-  //     getEvents().then((events) => {
-  //       const locationEvents = (updatedLocation === 'all') ?
-  //         events : events.filter((event) => event.location === updatedLocation);
-  //       // console.log("1st", locationEvents);
-  //       this.setState({
-  //         events: locationEvents,
-  //       });
-  //     });
-  //   } else if (eventCount) {
-  //     // console.log("eventcount", eventCount);
-  //     getEvents().then((events) => {
-  //       const locationEvents = (selectedLocation === 'all') ?
-  //         events : events.filter((event) => event.location === selectedLocation);
-  //       const filteredEvents = locationEvents.slice(0, eventCount);
-  //       // console.log("locationevents", locationEvents);
-  //       // console.log("filteredEvents", filteredEvents);
-  //       this.setState({
-  //         events: filteredEvents,
-  //         numberOfEvents: eventCount
-  //       });
-  //     });
-  //   }
-  // }
   updateEvents = (updatedLocation, eventCount) => {
     const { selectedLocation, numberOfEvents } = this.state;
-    console.log("numberofevents", numberOfEvents);
+    // console.log("numberofevents", numberOfEvents);
     if (updatedLocation) {
-      console.log("updatedLocation", updatedLocation);
+      // console.log("updatedLocation", updatedLocation);
       getEvents().then((events) => {
         const locationEvents = (updatedLocation === 'all') ?
           events : events.filter((event) => event.location === updatedLocation);
         const filteredEvents = locationEvents.slice(0, numberOfEvents);
-        console.log("1stnumberofevents", numberOfEvents);
-        console.log("1st", locationEvents);
+        // console.log("1stnumberofevents", numberOfEvents);
+        // console.log("1st", locationEvents);
         this.setState({
-          // events: locationEvents,
           events: filteredEvents,
           selectedLocation: updatedLocation
         });
       });
     } else {
-      console.log("eventcount", eventCount);
+      // console.log("eventcount", eventCount);
       getEvents().then((events) => {
         const locationEvents = (selectedLocation === 'all') ?
           events : events.filter((event) => event.location === selectedLocation);
         const filteredEvents = locationEvents.slice(0, eventCount);
-        console.log("locationevents", locationEvents);
-        console.log("filteredEvents", filteredEvents);
+        // console.log("locationevents", locationEvents);
+        // console.log("filteredEvents", filteredEvents);
         this.setState({
           events: filteredEvents,
           numberOfEvents: eventCount
@@ -131,6 +46,7 @@ class App extends Component {
       });
     }
   }
+
   // componentDidMount to make the API call and save the initial data to state:
   componentDidMount() {
     this.mounted = true;
@@ -142,9 +58,7 @@ class App extends Component {
           locations: extractLocations(events)
         });
       }
-      // this.updateEvents();
     });
-    // this.updateEvents();
   }
 
   componentWillUnmount() {
@@ -162,7 +76,6 @@ class App extends Component {
 
         <div className="number-input">
           <p className="input-label number-label">Show number of events:</p>
-          {/* <NumberOfEvents numberOfEvents={this.state.numberOfEvents} updateEventCount={this.updateEventCount} eventCount={this.state.eventCount} /> */}
           <NumberOfEvents numberOfEvents={this.state.numberOfEvents} updateEvents={this.updateEvents} eventCount={this.state.eventCount} />
         </div>
 
