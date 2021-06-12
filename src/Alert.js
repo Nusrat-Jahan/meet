9b8fb7a4ac8884093e6a691b5ffc5e748dffcbbc
Nusrat-Jahan <jahannusrat735@gmail.com>
2021-06-12 15:44:12 +0200

    Added Error and warning alert in Alert.js

--- src/Alert.js
+++ src/Alert.js
@@ -9,6 +9,8 @@ class Alert extends Component {
   getStyle = () => {
     return {
       color: this.color,
+      margin: this.margin,
+      // justifyContent: this.justifyContent
     };
   }
 
@@ -25,7 +27,25 @@ class InfoAlert extends Alert {
   constructor(props) {
     super(props);
     this.color = 'blue';
+    this.margin = '109px 0px 0px 0px';
   }
 }
 
-export { InfoAlert };
+class WarningAlert extends Alert {
+  constructor(props) {
+    super(props);
+    this.color = 'orange';
+  }
+}
+
+class ErrorAlert extends Alert {
+  constructor(props) {
+    super(props);
+    this.color = 'red';
+    this.margin = '235px 0px 0px 0px';
+    // this.justifyContent = 'center';
+    // alignItems: 'center'
+  }
+}
+
+export { InfoAlert, WarningAlert, ErrorAlert };
