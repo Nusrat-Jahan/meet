24287ff4de96b88440eea9d8806d7ef2a72d6fa6
Nusrat-Jahan <jahannusrat735@gmail.com>
2021-06-15 17:13:24 +0200

    Passed EventGenre props in App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import './nprogress.css';
 import PropTypes from 'prop-types';
 import { WarningAlert } from './Alert';
 import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import EventGenre from './EventGenre';
 
 class App extends Component {
   state = {
@@ -89,6 +90,7 @@ class App extends Component {
   }
 
   render() {
+    const { events } = this.state;
     return (
       <div className="App">
         <WarningAlert text={this.state.warningText} />
@@ -104,26 +106,37 @@ class App extends Component {
         </div>
 
         <h4>Events in each city</h4>
-
-        <ResponsiveContainer height={400} >
-          <ScatterChart
-            margin={{
-              top: 20, right: 20, bottom: 20, left: 20,
-            }}
-          >
-            <CartesianGrid />
-            <XAxis type="category" dataKey="city" name="city" />
-            <YAxis type="number" dataKey="number" name="number of events" allowDecimals={false} />
-            <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-            <Scatter data={this.getData()} fill="#8884d8" />
-          </ScatterChart>
-        </ResponsiveContainer>
-
+        <div className="data-vis-wrapper">
+          <EventGenre events={this.state.events} />
+          <ResponsiveContainer height={400} >
+            <ScatterChart
+              margin={{
+                top: 20,
+                right: 20,
+                bottom: 20,
+                left: 20,
+              }}
+            >
+              <CartesianGrid />
+              <XAxis
+                type="category"
+                dataKey="city"
+                name="city" />
+              <YAxis
+                type="number"
+                dataKey="number"
+                name="number of events"
+                allowDecimals={false} />
+              <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+              <Scatter data={this.getData()} fill="#8884d8" />
+            </ScatterChart>
+          </ResponsiveContainer>
+        </div>
         <EventList events={this.state.events} />
         {/* pass the state to EventList, as a prop of events */}
         {/* <Event showDetails={this.state.showDetails} /> */}
         {/* <WarningAlert text={this.state.warningText} /> */}
-      </div>
+      </div >
     );
   }
 }
